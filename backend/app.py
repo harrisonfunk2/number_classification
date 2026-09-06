724e2832536368457ae82a015d57495e7719a9e3
Harrison <162678129+harrisonfunk2@users.noreply.github.com>
2026-09-05 20:53:01 -0700

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from inference import pre_process_image, predict_digit
+
+
+app = FastAPI()
+
+origins = ['http://localhost:5500', 
+           'http://127.0.0.1:5500',]
+
+
+app.add_middleware(CORSMiddleware, 
+                   allow_origins=origins, 
+                   allow_credentials=True, 
+                   allow_methods=['*'], 
+                   allow_headers=['*'],)
+
+@app.get('/health')
+def health():
+    return {'status': 'running'}
+
+@app.post('/predict')
+async def predict(image: UploadFile = File(...)):
+    tensor_img = pre_process_image(image.file)
+
+    if tensor_img is None:
+        raise HTTPException(status_code=400, 
+                            detail='No digit detected')
+    
+    output = predict_digit(tensor_img)
+    return output
---
+++ backend/inference.py
@@ -0,0 +1,61 @@
+import torch
+from PIL import Image, ImageOps
+import torchvision.transforms as transforms
+from model import CNN
+
+
+device = torch.device('cpu')
+
+def load_model():
+    model = CNN().to(device)
+    state_dict = torch.load('best_cnn_model.pth', map_location=device,
+                            weights_only=True)
+    model.load_state_dict(state_dict)
+    model.eval()
+    return model
+
+MODEL = load_model()
+
+
+def pre_process_image(image_name):
+    img = Image.open(image_name)
+
+    # Convert to grayscale and invert
+    img = img.convert('L')
+    img = ImageOps.invert(img)
+
+    bounding_box = img.getbbox()
+    if bounding_box is None:
+        return None
+    img = img.crop(bounding_box)
+
+    # Resize, keep proportions
+    img.thumbnail((20, 20))
+
+    canvas = Image.new('L', (28, 28), 0)
+
+    # Center the resized digit
+    x = (28 - img.width) // 2
+    y = (28 - img.height) // 2
+    canvas.paste(img, (x, y))
+
+    tensor_img = transforms.ToTensor()(canvas)
+    tensor_img = tensor_img.unsqueeze(0)
+
+    return tensor_img
+
+
+def predict_digit(tensor_img):
+    if tensor_img is None:
+        return None
+
+    tensor_img = tensor_img.to(device)
+
+    with torch.no_grad():
+        logits = MODEL(tensor_img)
+        probabilities = torch.softmax(logits, dim=1)
+
+    confidence, predicted_number = torch.max(probabilities, dim=1)
+
+    return {'Confidence': confidence.item(),
+            'Predicted Number': predicted_number.item()}
